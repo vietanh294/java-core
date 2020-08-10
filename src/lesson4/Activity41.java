3585aa0531ec5969b33fa96de8f89b2f38c7c4f2
vietanh <nva.clsp@gmail.com>
2020-08-10 22:51:41 +0700

    first commit

---
+++ src/lesson4/Activity41.java
@@ -0,0 +1,18 @@
+package lesson4;
+
+import java.util.Scanner;
+
+public class Activity41 {
+    // comment 1 dong
+    /*
+    * Comment nhieu dong
+    *
+    * */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so nguyen n: ");
+        int n = scanner.nextInt();
+        System.out.println("So vua nhap la: " + n);
+    }
+}
